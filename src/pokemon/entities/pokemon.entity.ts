08e71f8b7f26e0f3dd5e25b95581c018faa9681d
Savecoders <pablopincay2003@gmail.com>
2024-02-26 11:59:44 -0500

    Learn: create entities and integrate mongoose

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,26 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+// Document is a type provided by Mongoose
+// that represents a single document in the database.
+
+export type PokemonDocument = HydratedDocument<Pokemon>;
+
+@Schema()
+export class Pokemon {
+  //! ID: not create id because it will be created by MongoDB
+  // @Prop() is a decorator provided by Mongoose
+  // that allows you to define the schema of a field.
+  //* is "bussiness logic" of the entity
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  num: number;
+}
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
