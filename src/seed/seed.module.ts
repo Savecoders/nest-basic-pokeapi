123f17d6ff7d3649674b33a4cebcd297bc20f774
Savecoders <pablopincay2003@gmail.com>
2024-02-28 10:30:20 -0500

    Learn: implement seed execute with axios

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
 // import { PokemonService } from 'src/pokemon/pokemon.service';
 // import { PokemonService } from 'src/pokemon/pokemon.service';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [HttpModule],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+    HttpModule,
+  ],
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,10 +3,17 @@ import { Injectable } from '@nestjs/common';
 // import { Observable } from 'rxjs';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { HttpService } from '@nestjs/axios';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @InjectModel(Pokemon.name)
+    private readonly pokeModel: Model<Pokemon>,
+  ) {}
 
   // findAll(): Observable<AxiosResponse<Pokemon[]>> {
   //   return this.httpService.axiosRef.get('http://localhost:3000/pokemon');
@@ -14,11 +21,12 @@ export class SeedService {
 
   async executeSeed() {
     const { data } = await this.httpService.axiosRef.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon',
+      'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0',
     );
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const num = +segments[segments.length - 2];
+      await this.pokeModel.create({ num, name });
     });
     return data.results;
   }
